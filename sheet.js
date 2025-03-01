33fa9416e034b022b3d4282077db3d998ac46606
Serdar Üşenmez <serdar.usenmez@gmail.com>
2025-03-01 14:44:08 +0300

    Attributes, skills and specializations are now all "Stats" with a "Type" key to differentiate. Implementing loading of sheet from JSON. Added temporary fonts.

--- sheet.js
+++ sheet.js
@@ -1,32 +1,107 @@
-class StatRow {
+class View {
+    constructor(element) {
+        this.element = element;
+    }
+
+    appendChild(child) {
+        this.element.appendChild(child.element);
+    }
+}
+
+class StatRow extends View {
     constructor(templateElement) {
-        this.element = templateElement.cloneNode(true);
-    }
-
-    setText(text) {
-        const paragraph = this.element.querySelector('p');
-        if (paragraph) {
-            paragraph.textContent = text;
-        }
-    }
-
-    appendTo(parent) {
-        parent.appendChild(this.element);
+        super(templateElement.cloneNode(true));
+        this.nameElement = this.element.querySelector('.stat-row-name');
     }
 
     setId(id) {
         this.element.id = id;
     }
+
+    setName(name) {
+        this.nameElement.textContent = name;
+    }
+}
+
+class SkillRow extends StatRow {
+    constructor(templateElement) {
+        super(templateElement);
+        this.nameElement.classList.add('stat-row-name-skill');
+    }
+}
+
+class SpecRow extends StatRow {
+    constructor(templateElement) {
+        super(templateElement);
+        this.nameElement.classList.add('stat-row-name-spec');
+    }
+}
+
+function pipsToDice(totalPips) {
+    let dice = Math.floor(totalPips / 3);
+    let pips = totalPips % 3;
+
+    if (pips === 0)
+        return dice + "D";
+
+    return dice + "D+" + pips;
+}
+
+function calculateStatTotals(character) {
+
 }
 
 // Fill sheet after DOM loads
 document.addEventListener("DOMContentLoaded", () => {
-    const statView = document.getElementById('stat-view');
+    const statView = new View(document.getElementById('stat-view'));
     const statRowTemplate = document.getElementById('template-stat-row');
     const statModifierRowTemplate = document.getElementById('template-stat-modifier-row');
-    const statRow = new StatRow(statRowTemplate);
-    statView.appendChild(statRow.element);
-    const statRow2 = new StatRow(statRowTemplate);
-    statView.appendChild(statRow2.element);
-    statRow2.element.querySelector('.stat-row-name').textContent = 'Strength';
+
+    fetch('hecreus.json')
+        .then(response => { return response.json(); })
+        .then(data => {
+            let rows = [];
+
+            data.Stats.forEach(element => {
+                let row = null
+
+                switch (element.Type) {
+                    case "Attribute":
+                        row = new StatRow(statRowTemplate);
+                        row.setId("attribute-" + element.Name);
+                        row.setName(element.Name);
+
+                        statView.appendChild(row);
+
+                        rows[element.Name] = row;
+                        break;
+
+                    case "Skill":
+                        row = new SkillRow(statRowTemplate);
+                        row.setId("skill-" + element.Name);
+                        row.setName(element.Name);
+
+                        let attributeRow = rows[element.Base];
+                        attributeRow.appendChild(row);
+
+                        rows[element.Name] = row;
+                        break;
+
+                    case "Specialization":
+                        row = new SpecRow(statRowTemplate);
+                        row.setId("spec-" + element.Name);
+                        row.setName(element.Name);
+
+                        let skillRow = rows[element.Base];
+                        skillRow.appendChild(row);
+
+                        rows[element.Name] = row;
+                        break;
+                }
+
+                if (row !== null) {
+
+                }
+            });
+        });
 });
