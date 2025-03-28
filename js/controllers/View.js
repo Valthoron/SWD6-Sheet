f5b523bf1341133d3590bbae3c641c21dbcfe942
Serdar Üşenmez <serdar.usenmez@gmail.com>
2025-03-28 12:11:06 +0300

    Added more options for View parent/child manipulation.

--- js/controllers/View.js
+++ js/controllers/View.js
@@ -2,7 +2,8 @@ export class View {
     #initialized = false;
 
     _element = null;
-    //_children = [];
+    _parent = null;
+    _children = [];
 
     constructor(element) {
         this._element = element;
@@ -29,7 +30,40 @@ export class View {
     }
 
     appendChild(child) {
+        if (!(child instanceof View)) {
+            throw new Error("Child must be an instance of View.");
+        }
+
         this._element.appendChild(child._element);
-        //this._children.push(child);
+        this._children.push(child);
+        child._parent = this;
+    }
+
+    appendToParent(parent) {
+        if (!(parent instanceof View)) {
+            throw new Error("Parent must be an instance of View.");
+        }
+
+        parent.appendChild(this);
+    }
+
+    removeChild(child) {
+        if (!(child instanceof View)) {
+            throw new Error("Child must be an instance of View.");
+        }
+
+        if (!this._children.includes(child)) {
+            throw new Error("Child is not a child of this view.");
+        }
+
+        this._element.removeChild(child._element);
+        this._children = this._children.filter(c => c !== child);
+        child._parent = null;
+    }
+
+    removeFromParent() {
+        if (this._parent) {
+            this._parent.removeChild(this);
+        }
     }
 }
